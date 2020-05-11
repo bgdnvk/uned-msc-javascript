3cb8bc0050dc9a806d219a42efa01c0f8201bdf9
bgdnvk <bnovykov@gmail.com>
2020-05-11 23:55:22 +0200

    3-3 hecho

--- 03_Ejercicios_Material/js/Ejercicio_3-3.js
+++ 03_Ejercicios_Material/js/Ejercicio_3-3.js
@@ -1,6 +1,10 @@
 
 // Escribir en este fichero la función calculoMax2() pedida en el enunciado
-	
+function calculoMax2(num1, num2){
+    let x = parseInt(num1);
+    let y = parseInt(num2);
+    return Math.max(x,y);
+}	
 
 
 
