fa9e217b1751e059a4af0ef4667823b7f7566675
bgdnvk <bnovykov@gmail.com>
2020-05-11 23:41:19 +0200

    3-2 hecho

--- 03_Ejercicios_Material/js/Ejercicio_3-1.js
+++ 03_Ejercicios_Material/js/Ejercicio_3-1.js
@@ -1,6 +1,11 @@
 // Escribir en este fichero las funciones mensaje1() y mensaje2()
+function mensaje1(){
+    document.getElementById("texto").innerHTML = "<p> Has pulsado BOTÓN 1</p>"
+}
 
-
+function mensaje2(){
+    document.getElementById("texto").innerHTML = "<p> Has pulsado BOTÓN 2</p>"
+}
 
 
 
--- 03_Ejercicios_Material/js/Ejercicio_3-2.js
+++ 03_Ejercicios_Material/js/Ejercicio_3-2.js
@@ -1,8 +1,15 @@
 // Escribir en este fichero las funciones factorial() y convertir() pedidas en el enunciado
+//recursion+ternary
+function factorial(x){
+    let miNum = parseFloat(x);
+    return (miNum === 1? 1:
+        miNum* factorial(miNum-1)
+        );
+}
 
-
-
-
+function convertir(x){
+    return parseFloat(x*9/5+32);
+}
 
 
 
