142d8d3d935c6141376bc919b70f14420bf6404b
bgdnvk <bnovykov@gmail.com>
2020-06-17 01:45:51 +0200

    4-5

--- 04_Ejercicios_Material/js/Ejercicio_4-5.js
+++ 04_Ejercicios_Material/js/Ejercicio_4-5.js
@@ -4,7 +4,18 @@
 function obtenerLetra(){
 
 		//  TRWAGMYFPDXBNJZSQVHLCKET  
+	const dniNum = document.querySelector("form input");
+	const letra = letraDNI(dniNum.value);
 
+	dniNum.value += letra;
+	/**
+	 * usando getElementById sería lo mismo pero hay que añádir
+	 * la id dentro del html tag y luego dniNum = doc.getElebyId("idAñadida")
+	 */
 }
 
-
+function letraDNI(dni){
+	let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
+	let numeroLetra = dni%23;
+	return cadena.charAt(numeroLetra)
+}
