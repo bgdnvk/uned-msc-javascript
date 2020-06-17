e317f5332129969dd3c80dd16b88c7dec81f98e6
bgdnvk <bnovykov@gmail.com>
2020-06-17 04:31:19 +0200

    4-6

--- 04_Ejercicios_Material/js/Ejercicio_4-6.js
+++ 04_Ejercicios_Material/js/Ejercicio_4-6.js
@@ -1,17 +1,19 @@
 // JavaScript Document
-//arrays donde se guardan las imagenes y sus textos
+//array donde se guardan las imagenes
 const imgArr = [];
-const textoArr = [];
+
+//no se usa el array ver L44
+//const textoArr = [];
 
 function cambiarFoto(){
         
         let textGet = document.getElementById("titulos");
-        
         let imageGet = document.getElementById("facultades").getAttribute("src");
+        //se pasan los nodos de la página a editar
         imageNueva(imageGet, textGet);
 
+        //se actualiza la página cada 2s
         tiempo=setTimeout("cambiarFoto()", 2000);
-
 }
 
 //se pone la imagen nueva
@@ -19,9 +21,11 @@ function imageNueva(imageGet, textGet){
     //calculo aleatorio de la posicion del array
     //donde estan todas las imgs
     let numAleatorio = Math.floor(Math.random()*imgArr.length);
+    //se coge la imagen(dir de la imagen) del array con el num aleatorio
     let imagenAleatoria = imgArr[numAleatorio];
-    
-    let textoAponer = numAleatorio;
+
+    //variable no se usa: ver línea 44
+    //let textoAponer = textoArr[numAleatorio];
 
     console.log("img puesta: "+imageGet);
     console.log("img a poner: "+imagenAleatoria);
@@ -31,11 +35,18 @@ function imageNueva(imageGet, textGet){
         imageNueva(imageGet,textGet);
         
     } else{
+        //se carga la direccion de la imagen en ID facultades
         document.getElementById("facultades").setAttribute("src", imagenAleatoria);
-        
+        //se recorta y edita (si es necesario) el texto de la imagen que cogemos
+        //se carga en ID titulos
         textGet.textContent = `Facultad de ${cortarTexto(imagenAleatoria)}`;
 
-
+        /**  si el nombre de las facultades (al cargarlas, usando cargarFacultad())
+        * no estuviese alterado (si las imagenes tuviesen los nombres correctamente editados
+        * por ejemplo industriales.jpg en vez de industriales130.jpg)
+        * se podría usar el array textoArr y la siguiente carga a HTML: 
+        */
+        // textGet.textContent = `${textoAponer}`;
     }
 }
 
@@ -53,16 +64,20 @@ function cortarTexto(texto){
     }
     return texto;
 }
+//------------ se cargan las imagenes en el array imgArr[]----
 
 //funcion para cargar la img
 //solo sabiendo el nombre de la facultad
 function cargarFacultades(facultad){
     imgArr.push(`img/foto${facultad}.jpg`);
-    textoArr.push(`Facultad de ${facultad}`);
+
+    //array de textos no se usa: ver linea 44
+    //textoArr.push(`Facultad de ${facultad}`);
 }
-//se cargan tas las imagenes dentro del array
-//dado su nombre correspondiente
-//IIFE
+
+// se cargan tas las imagenes dentro del array
+// dado su nombre correspondiente
+// IIFE
 (function cargarImgs(){
     cargarFacultades("Ciencias");
     cargarFacultades("Derecho");
