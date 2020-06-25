bf5a79bd324f7b49e29a43a6b6c2971e0843f9c1
bgdnvk <bnovykov@gmail.com>
2020-06-25 05:51:16 +0200

    5-1

--- 05_Ejercicios_Material/js/Ejercicio_5-1.js
+++ 05_Ejercicios_Material/js/Ejercicio_5-1.js
@@ -1,10 +1,28 @@
 // JavaScript Document
+/**
+ * arrays con instrucciones según ejercicio
+ * se puediese haber usado [x,x,x].forEach
+ * pero con las variables se lee mejor
+ */
+const encima = ["mouseover", "Donald_2.png"];
+const retira = ["mouseout", "Donald_1.png"];
+const click = ["click", "Goofi_1.png"];
+const dblclick = ["dblclick", "Goofi_2.png"];
 
+const instrucciones = [encima, retira, click, dblclick];
 
+window.addEventListener('load', function() {
+    //se identifica la imagen a cambiar
+    const cambioImg = document.getElementById('cambio');
+    //se usa un foreach para iterar sobre el array de instrucciones
+    instrucciones.forEach(function(element){
+         //se crean event listeners con sus instrucciones específicas
+        cambioImg.addEventListener(element[0], function(e){
+            this.src = `img/${element[1]}`;
+        });
+    })
 
-
-
-
+})
 
 
 
