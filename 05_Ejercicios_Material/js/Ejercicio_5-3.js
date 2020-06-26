d0f347ce2a84b0bbe675b88e1741d1ed6196b911
bgdnvk <bnovykov@gmail.com>
2020-06-26 07:30:23 +0200

    5-3casi

--- 05_Ejercicios_Material/js/Ejercicio_5-3.js
+++ 05_Ejercicios_Material/js/Ejercicio_5-3.js
@@ -1,3 +1,72 @@
 // JavaScript Document
 
 
+
+
+
+
+//si no se pone el script al final de html se tiene que usar onload
+window.onload = function(){
+    //cogemos la casilla de textodni
+    const textDniForm = document.getElementById("textoDNI");
+    //más facil con <input type="number"> en html
+    //leemos las teclas y solo se permiten los números
+    textDniForm.addEventListener("input", e =>{
+        let value = textDniForm.value;
+        //regex para los números
+        let rgx = new RegExp(/^[0-9]*$/);
+        //valor del texto antes de agregar la última tecla
+        let txt = e.currentTarget.value.slice(0,-1);
+        //si no es un número se vuelve a cargar el texto que había
+        //antes de introducir el nuevo char
+        (!rgx.test(e.target.value)) && (textDniForm.value = txt);
+
+    })
+    //se leen los números y se calcula la letra
+    textDniForm.addEventListener("keyup", e => {
+        console.log(e);
+        console.log(e.key);
+        console.log(e.code);
+        
+        // console.log(Number(e.key));
+        let longitud = textDniForm.value.length;
+        //valor dentro del formulario
+        let value = textDniForm.value;
+        let rgx = new RegExp(/^[0-9]*$/);
+        //se escribe la letra si input son 8 numeros
+        (longitud  === 8 && rgx.test(value)) && cambiaEstado();
+        //si tenemos ya el dni escrito y pulsamos backspace dejamos solo los números
+        (e.key === "Backspace" && longitud >= 8) && (textDniForm.value = value.slice(0,8))
+    })
+}
+
+
+function cambiaEstado() {
+    escribirLetra();
+    cambiarTexto();
+    cambiarColorFondo();
+    
+}
+
+function escribirLetra(){
+
+    var dni = document.getElementById("textoDNI").value;
+    var letras="TRWAGMYFPDXBNJZSQVHLCKET"
+    var posicion=dni%23;
+    letra = letras.charAt(posicion);
+    // document.getElementById("textoDNI").value = dni+"-"+letra;
+    //cambiado para parecer más al ejercicio del ejemplo
+    document.getElementById("textoDNI").value +=" - "+letra;
+
+}
+
+function cambiarTexto(){
+    document.getElementById("capaTextoId").innerHTML = "DNI completo";
+}
+function cambiarColorFondo() {
+    //cambiado el color para parecerse al de ejercicio
+    document.getElementById("form").style.backgroundColor ="#00b0ff";
+    // y se cambia también el color de background
+    document.body.style.backgroundColor = "#0081cb";
+    //he usado la paleta de material design
+}
