9ae9d038ce5be52a79a83f51f12c6660c09ddaad
bgdnvk <bnovykov@gmail.com>
2020-06-27 05:25:21 +0200

    b4 cleaning

--- 05_Ejercicios_Material/js/Ejercicio_5-4.js
+++ 05_Ejercicios_Material/js/Ejercicio_5-4.js
@@ -19,14 +19,79 @@ const player = {
 }
 //función para actualizar el tiempo de reproducción
 //se coge solo una décima con toFixed()
-let actualizaTiempo = () => tiempoTxt.innerHTML = player.getTime().toFixed(1);
+let actualizaTiempo = () =>{
+   let tiempo = tiempoTxt.innerHTML = player.getTime().toFixed(0);
+   //solo actualizar los datos cuando el video se reproduzca
+   if(!video.paused){
+       console.log(tiempo);
+       console.log(typeof(tiempo));
+       
+        arrayTiempos.forEach(e => {
+            // console.log("tiempo es: "+Number(tiempo));
+            // console.log(typeof(tiempo));
+            
+            // console.log("e es : "+Number(e));
+            // console.log(typeof(e));
+            
+            
+            
+            if(Number(e) === Number(tiempo)){
+                console.log("##########");
+                console.log("tiempo es: "+tiempo);
+                console.log("e es : "+e);
+                
+                actualizarImagen(arrayImagenes[arrayTiempos.indexOf(e)]);
+            }
+            // Number(e) === Number(tiempo) && actualizarImagen(arrayTiempos[e])
+        })
+
+    //    switch(tiempo){
+    //        case "0.0":
+    //            actualizarImagen(arrayImagenes[0]);
+    //            break;
+    //         case "7.0":
+    //             actualizarImagen(arrayImagenes[1]);
+    //             break;
+    //    }
+   }
+   
+   
+} 
 //se pone el intervalo de actualización
 setInterval(actualizaTiempo, 100);
 
-
+//se cargan los eventListeners para clicks usando el objeto player
 document.getElementById("play").addEventListener("click", player.play);
 document.getElementById("small").addEventListener("click", player.small);
 document.getElementById("normal").addEventListener("click", player.normal);
 document.getElementById("big").addEventListener("click", player.big);
 
+//se cargan 2 arrays con las imagenes: su posición y el path
+const arrayImagenes = [];
+const arrayTiempos = ["00","07",12,17,23,27,31,32];
 
+(function cargarArrays(){
+    
+    for (let i = 0; i<8; i++){
+        arrayImagenes.push(
+            `img/Imagen0${i+1}_0${arrayTiempos[i]}.jpg`
+        )
+    }
+    // return arrayImagenes;
+})();
+console.log(arrayImagenes);
+
+// const arrays2 = [00,07,12,17,23,27,31,32];
+// let cargarArrays2 = () =>{
+//    let newArr= arrays2.map(x => `img/Imagen0${arrays2.indexOf(x)+1}_0${arrays2.indexOf(x)}.jpg`);
+//    console.log(newArr);
+   
+// }
+// cargarArrays2();
+
+
+
+
+function actualizarImagen(x){
+    diapositiva.src = x;
+}
